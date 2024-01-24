0c74113bc652fe1e8fdc576f751756608a212d95
Vit-Alex <daevennareiel@gmail.com>
2024-01-24 23:00:14 +0300

    Описание изменений

---
+++ configuration.py
@@ -0,0 +1,3 @@
+URL_SERVICE = " https://ab7d3d37-db58-4d8d-a921-67f94c0375e9.serverhub.praktikum-services.ru"
+CREATE_NEW_ORDER = "/api/v1/orders"
+RECEIVING_ORDER_BY_ID = "/api/v1/orders/track"
---
+++ data.py
@@ -0,0 +1,13 @@
+order_body = {
+    "firstName": "Давид",
+    "lastName": "Валенте",
+    "address": "Рим, 11 дом.",
+    "metroStation": 8,
+    "phone": "+7 977 980 98 98",
+    "rentTime": 6,
+    "deliveryDate": "2024-08-09",
+    "comment": "Il mi Amore",
+    "color": [
+        "BLACK"
+    ]
+}
